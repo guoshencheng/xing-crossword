b945ea616b92b5ea37dcff892b4feb43fc9f2ee9
Sherlock Yao <yaosherlock@gmail.com>
2014-07-10 11:18:43 +0800

    fix build error

---
+++ XingCrossword/Item+Utility.h
@@ -0,0 +1,13 @@
+//
+//  Item+Utility.h
+//  XingCrossword
+//
+//  Created by Sherlock on 7/10/14.
+//  Copyright (c) 2014 apple. All rights reserved.
+//
+
+#import "Item.h"
+
+@interface Item (Utility)
+
+@end
---
+++ XingCrossword/Item+Utility.m
@@ -0,0 +1,13 @@
+//
+//  Item+Utility.m
+//  XingCrossword
+//
+//  Created by Sherlock on 7/10/14.
+//  Copyright (c) 2014 apple. All rights reserved.
+//
+
+#import "Item+Utility.h"
+
+@implementation Item (Utility)
+
+@end
